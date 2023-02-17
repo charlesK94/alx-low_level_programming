3f66cd0f787c06eb248c4f083f590fc176e24b29
charlesK94 <charleskioko94@gmail.com>
2023-02-17 12:37:21 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
 
  * main - prints all single digit numbers of base
@@ -17,9 +16,7 @@ int main(void)
 
         int n;
 
-
         for (n = 0; n < 10; n++)
-
                 printf("%d", n);
 
         printf("\n");
