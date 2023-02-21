3382c50428e0736853461e8487b5c25fe6f2eaa8
charlesK94 <charleskioko94@gmail.com>
2023-02-21 13:20:02 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,23 @@
-#include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 int main(void)
+
 {
-    char str[] = "_putchar\n";
-    int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-        _putchar(str[i]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 
-    return (0);
 }
-
